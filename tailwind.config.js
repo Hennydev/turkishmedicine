67b8137873bd72799bf0affdb24eafac4e3500bf
techysuite <63807149+angelbendgr8-dev@users.noreply.github.com>
2023-12-06 11:53:30 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Mulish: ["Mulish", "sans-serif"],
+      },
+      animation: {
+        "text-slide":
+          "text-slide 12.5s cubic-bezier(0.83, 0, 0.17, 1) infinite",
+      },
+      keyframes: {
+        "text-slide": {
+          "0%, 16%": {
+            transform: "translateY(0%)",
+          },
+          "20%, 36%": {
+            transform: "translateY(-16.66%)",
+          },
+          "40%, 56%": {
+            transform: "translateY(-33.33%)",
+          },
+          "60%, 76%": {
+            transform: "translateY(-50%)",
+          },
+          "80%, 96%": {
+            transform: "translateY(-66.66%)",
+          },
+          "100%": {
+            transform: "translateY(-83.33%)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("tw-elements/dist/plugin.cjs"), require("preline/plugin")],
+
+  darkMode: "class",
+};
